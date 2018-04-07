d1640427c6d6edd9155d8b284dc50df498d8301b
halaz.lazlo@gmail.com <halaz.lazlo@gmail.com>
2018-04-07 17:57:18 +0200

    textarea placeholder added

--- src/Resources/views/form/components/input-textarea.php
+++ src/Resources/views/form/components/input-textarea.php
@@ -1,1 +1,6 @@
-<textarea name="<?php echo $input_id; ?>" id="<?php echo $input_id; ?>" class="form__input form__input--textarea"></textarea>
+<textarea
+    name="<?php echo $input_id; ?>" id="<?php echo $input_id; ?>"
+    class="form__input form__input--textarea"
+    <?php if (isset($input['placeholder'])): ?>
+        placeholder="<?php echo $input['placeholder']; ?>"
+    <?php endif ?>></textarea>
