446c65556b13e509c3eff5002f768ec4f97b9a9c
halaz.lazlo@gmail.com <halaz.lazlo@gmail.com>
2018-04-07 13:12:37 +0200

    column fixes

--- src/Resources/views/shortcodes/vc-column-inner.php
+++ src/Resources/views/shortcodes/vc-column-inner.php
@@ -1,19 +1,46 @@
 <?php
 
+$columnWidths = array(
+    '1/12'  => 'col-sm-1',
+    '1/6'   => 'col-sm-2',
+    '1/4'   => 'col-sm-3',
+    '1/3'   => 'col-sm-4',
+    '5/12'  => 'col-sm-5',
+    '1/2'   => 'col-sm-6',
+    '7/12'  => 'col-sm-7',
+    '2/3'   => 'col-sm-8',
+    '3/4'   => 'col-sm-9',
+    '5/6'   => 'col-sm-10',
+    '11/12' => 'col-sm-11',
+    '1/1'   => 'col-sm-12',
+);
+
 $classes = [];
-$classesArr = explode(' ', $atts['offset']);
 
-foreach ($classesArr as $class) {
-    $convertedClass = str_replace('vc_', '', $class);
+// width
+if (isset($atts['width'])) {
+    $classes[] = $columnWidths[$atts['width']];
+}
 
-    if (strpos($convertedClass, 'offset') !== false) {
-        $convertedClass = str_replace('col-', '', $convertedClass);
-        $convertedClass = str_replace('offset-', '', $convertedClass);
+// offset
+if (isset($atts['offset'])) {
+    $classesArr = explode(' ', $atts['offset']);
+    foreach ($classesArr as $class) {
+        $convertedClass = str_replace('vc_', '', $class);
 
-        $convertedClass = 'offset-'.$convertedClass;
+        if (strpos($convertedClass, 'offset') !== false) {
+            $convertedClass = str_replace('col-', '', $convertedClass);
+            $convertedClass = str_replace('offset-', '', $convertedClass);
+
+            $convertedClass = 'offset-'.$convertedClass;
+        }
+
+        $classes[] = $convertedClass;
     }
+}
 
-    $classes[] = $convertedClass;
+if (!isset($atts['width']) && !isset($atts['offset'])) {
+    $classes[] = 'col-lg-12';
 }
 
 $col = [
