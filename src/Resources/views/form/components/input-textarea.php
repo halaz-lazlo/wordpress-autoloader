148d436ab841836be96c5f55065ede6318f96a5a
halaz.lazlo@gmail.com <halaz.lazlo@gmail.com>
2018-08-11 23:43:12 +0200

    textarea fix

--- src/Resources/views/form/components/input-textarea.php
+++ src/Resources/views/form/components/input-textarea.php
@@ -1,6 +1,7 @@
 <textarea
-    name="<?php echo $input_id; ?>" id="<?php echo $input_id; ?>"
+    name="<?php echo $input_id; ?>"
+    id="<?php echo $input_id; ?>"
     class="form__input form__input--textarea"
     <?php if (isset($input['placeholder'])): ?>
         placeholder="<?php echo $input['placeholder']; ?>"
-    <?php endif ?>></textarea>
+    <?php endif ?>><?php echo $input['value']; ?></textarea>
