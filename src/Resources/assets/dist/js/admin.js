4ec22f571d02bb802e3fb836245917b79db005ad
halaz.lazlo@gmail.com <halaz.lazlo@gmail.com>
2018-10-22 21:29:44 +0200

    form imgs

--- src/Resources/assets/dist/js/admin.js
+++ src/Resources/assets/dist/js/admin.js
@@ -2,6 +2,7 @@
 'use strict';
 
 jQuery(document).ready(function ($) {
+  // img
   $('.input-img .input-img__button').on('click', function (e) {
     e.preventDefault();
 
@@ -41,6 +42,75 @@ jQuery(document).ready(function ($) {
     wrap.removeClass('input-img--has-image');
     input.val('');
     thumb.html('');
+  });
+
+  // imgs
+  $('.input-imgs__btn').on('click', function (e) {
+    e.preventDefault();
+
+    var btn = $(this);
+    var wrap = $(this).parents('.input-imgs');
+    var input = wrap.find('.input-imgs__val');
+    var thumbs = wrap.find('.input-imgs__thumbs');
+
+    var frame = wp.media({
+      title: 'Select image',
+      button: {
+        text: 'Select'
+      },
+      multiple: 'add'
+    });
+
+    // open
+    frame.on('open', function () {
+      var selection = frame.state().get('selection');
+      var imgIds = input.val(); // the id of the image
+      if (imgIds) {
+        var imgIds = imgIds.split(',');
+
+        imgIds.forEach(function (imgId) {
+          selection.add(wp.media.attachment(imgId));
+        });
+      }
+    });
+
+    // select
+    frame.on('select', function () {
+      var selection = frame.state().get('selection');
+      var imgIds = [];
+
+      // add thumbs
+      thumbs.html('');
+      selection.map(function (attachment) {
+        attachment = attachment.toJSON();
+
+        // append img
+        thumbs.append($('<div data-id="' + attachment.id + '" class="input-imgs__thumb"><img class="input-imgs__thumb-img" src="' + attachment.url + '" /><div class="input-imgs__thumb-remove">X</div></div>'));
+
+        // save id
+        imgIds.push(attachment.id);
+      });
+
+      // set value
+      input.val(imgIds.join(','));
+    });
+
+    // remove
+
+    frame.open();
+  });
+
+  $('.input-imgs__thumb-remove').on('click', function (e) {
+    var wrap = $(this).parents('.input-imgs');
+    var input = wrap.find('.input-imgs__val');
+    var img = $(this).parents('.input-imgs__thumb');
+    var id = img.data('id').toString();
+
+    var ids = input.val().split(',');
+    ids.splice(ids.indexOf(id), 1);
+
+    input.val(ids.join(','));
+    img.remove();
   });
 });
 
---
+++ src/Resources/views/form/components/input-imgs.php
@@ -0,0 +1,27 @@
+<div class="input-imgs">
+    <input
+        id="<?php echo $input_id; ?>"
+        class="input-imgs__val hidden"
+        name="<?php echo $input_id; ?>"
+        value="<?php echo $input['value']; ?>" />
+
+
+    <div class="input-imgs__thumbs">
+        <?php if($input['value']) :  ?>
+            <?php foreach(explode(',', $input['value']) as $imgId) : ?>
+                <div data-id="<?php echo $imgId; ?>" class="input-imgs__thumb">
+                    <img
+                        class="input-imgs__thumb-img"
+                        src="<?php echo wp_get_attachment_image_src($imgId, 'sm')[0] ?>">
+                    <div class="input-imgs__thumb-remove">X</div>
+                </div>
+            <?php endforeach; ?>
+        <?php endif;  ?>
+    </div>
+
+    <div class="input-imgs__footer">
+        <button class="input-imgs__btn button">
+            Add
+        </button>
+    </div>
+</div>
